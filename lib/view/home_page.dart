f4b56626245ce4c78c8f266ea37820c41bd6c900
ElgizCebrayilov992 <elgizcebrayilov1992@gmail.com>
2021-03-07 07:22:41 -0800

    task

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fonibotask/models/note.dart';
 import 'package:fonibotask/services/json_service.dart';
-import 'package:fonibotask/wdigets/list_wdiget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -14,9 +13,9 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    noteList = JsonService().getJsonPlaceHolderData();
     // TODO: implement initState
     super.initState();
+    noteList = JsonService().getJsonPlaceHolderData();
   }
 
   @override
@@ -54,127 +53,6 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-
-//          Row(
-//            //  mainAxisAlignment: MainAxisAlignment.spaceAround,
-//            children: [
-//              //color
-//              Container(
-//                width: 10,
-//                height: 70,
-//                decoration: BoxDecoration(
-//                    color: ColorGenerator().randomCcolors(),
-//                    borderRadius: BorderRadius.circular(10)),
-//              ),
-//
-//              Container(
-//                alignment: Alignment.centerLeft,
-//                margin: EdgeInsets.only(left: 5, right: 5),
-//                padding: EdgeInsets.only(left: 5, right: 5),
-//                height: 70,
-//                width: MediaQuery.of(context).size.width - 36,
-//                decoration: BoxDecoration(
-//                  color: Colors.grey.withOpacity(0.2),
-//                  borderRadius: BorderRadius.circular(10),
-//                ),
-//                child: ListTile(
-//                  title: Text("sdsdfdsfd"),
-//                  subtitle: Text("Created at: ${"DSFdsfdsf"}"),
-//                  trailing: Radio(
-//                    value: 1,
-//                    groupValue: val,
-//                    onChanged: (value) {
-//                      setState(() {
-//                        debugPrint("groupValue: $val" + "   NoteID: $value");
-//                        val = value;
-//                        debugPrint("groupValue: $val" + "   NoteID: $value");
-//                      });
-//                    },
-//                  ),
-//                ),
-//              ),
-//            ],
-//          ),
-//          Row(
-//            //  mainAxisAlignment: MainAxisAlignment.spaceAround,
-//            children: [
-//              //color
-//              Container(
-//                width: 10,
-//                height: 70,
-//                decoration: BoxDecoration(
-//                    color: ColorGenerator().randomCcolors(),
-//                    borderRadius: BorderRadius.circular(10)),
-//              ),
-//
-//              Container(
-//                alignment: Alignment.centerLeft,
-//                margin: EdgeInsets.only(left: 5, right: 5),
-//                padding: EdgeInsets.only(left: 5, right: 5),
-//                height: 70,
-//                width: MediaQuery.of(context).size.width - 36,
-//                decoration: BoxDecoration(
-//                  color: Colors.grey.withOpacity(0.2),
-//                  borderRadius: BorderRadius.circular(10),
-//                ),
-//                child: ListTile(
-//                  title: Text("sdsdfdsfd"),
-//                  subtitle: Text("Created at: ${"DSFdsfdsf"}"),
-//                  trailing: Radio(
-//                    value: 2,
-//                    groupValue: val,
-//                    onChanged: (value) {
-//                      setState(() {
-//                        debugPrint("groupValue: $val" + "   NoteID: $value");
-//                        val = value;
-//                        debugPrint("groupValue: $val" + "   NoteID: $value");
-//                      });
-//                    },
-//                  ),
-//                ),
-//              ),
-//            ],
-//          ),
-//          Row(
-//            //  mainAxisAlignment: MainAxisAlignment.spaceAround,
-//            children: [
-//              //color
-//              Container(
-//                width: 10,
-//                height: 70,
-//                decoration: BoxDecoration(
-//                    color: ColorGenerator().randomCcolors(),
-//                    borderRadius: BorderRadius.circular(10)),
-//              ),
-//
-//              Container(
-//                alignment: Alignment.centerLeft,
-//                margin: EdgeInsets.only(left: 5, right: 5),
-//                padding: EdgeInsets.only(left: 5, right: 5),
-//                height: 70,
-//                width: MediaQuery.of(context).size.width - 36,
-//                decoration: BoxDecoration(
-//                  color: Colors.grey.withOpacity(0.2),
-//                  borderRadius: BorderRadius.circular(10),
-//                ),
-//                child: ListTile(
-//                  title: Text("sdsdfdsfd"),
-//                  subtitle: Text("Created at: ${"DSFdsfdsf"}"),
-//                  trailing: Radio(
-//                    value: 3,
-//                    groupValue: val,
-//                    onChanged: (value) {
-//                      setState(() {
-//                        debugPrint("groupValue: $val" + "   NoteID: $value");
-//                        val = value;
-//                        debugPrint("groupValue: $val" + "   NoteID: $value");
-//                      });
-//                    },
-//                  ),
-//                ),
-//              ),
-//            ],
-//          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: _placeHolderDatasWigdet,
@@ -189,11 +67,55 @@ class _HomePageState extends State<HomePage> {
         builder: (context, AsyncSnapshot<List<Note>> snapshot) {
           if (snapshot.hasData) {
             return Column(
-              children: snapshot.data
-                  .map(
-                    (e) => ListWdiget(e, 1),
-                  )
-                  .toList(),
+              children: snapshot.data.map(
+                (e) {
+                  return Container(
+                    margin: EdgeInsets.only(bottom: 5),
+                    child: Row(
+                      //  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        //color
+                        Container(
+                          width: 10,
+                          height: MediaQuery.of(context).size.height * 0.10,
+                          decoration: BoxDecoration(
+                              color: e.color,
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          margin: EdgeInsets.only(left: 5, right: 5),
+                          padding: EdgeInsets.only(left: 5, right: 5),
+                          height: MediaQuery.of(context).size.height * 0.10,
+                          width: MediaQuery.of(context).size.width * 0.90,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            title: Text(e.title),
+                            subtitle: Text("Created at: ${e.createdAt}"),
+                            trailing: Radio(
+                              value: e.id,
+                              groupValue: val,
+                              onChanged: (value) {
+                                setState(() {
+                                  debugPrint(
+                                      "groupValue: $val" + "   NoteID: $value");
+                                  val = value;
+                                  debugPrint(
+                                      "groupValue: $val" + "   NoteID: $value");
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ).toList(),
             );
           } else {
             return Center(child: CircularProgressIndicator());
@@ -201,51 +123,3 @@ class _HomePageState extends State<HomePage> {
         },
       );
 }
-/*
-* Container(
-                      margin: EdgeInsets.only(bottom: 5),
-                      child: Row(
-                        //  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          //color
-                          Container(
-                            width: 10,
-                            height: MediaQuery.of(context).size.height * 0.10,
-                            decoration: BoxDecoration(
-                                color: e.color,
-                                borderRadius: BorderRadius.circular(10)),
-                          ),
-
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            margin: EdgeInsets.only(left: 5, right: 5),
-                            padding: EdgeInsets.only(left: 5, right: 5),
-                            height: MediaQuery.of(context).size.height * 0.10,
-                            width: MediaQuery.of(context).size.width * 0.90,
-                            decoration: BoxDecoration(
-                              color: Colors.grey.withOpacity(0.2),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: ListTile(
-                              title: Text(e.title),
-                              subtitle: Text("Created at: ${e.createdAt}"),
-                              trailing: Radio(
-                                value: e.id,
-                                groupValue: val,
-                                onChanged: (value) {
-                                  setState(() {
-                                    debugPrint("groupValue: $val" +
-                                        "   NoteID: $value");
-                                    val = value;
-                                    debugPrint("groupValue: $val" +
-                                        "   NoteID: $value");
-                                  });
-                                },
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-* 
-* */
